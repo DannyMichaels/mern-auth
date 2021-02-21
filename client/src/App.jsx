6b24a96aa5fdf3b3195c255d38516b7b8a83189a
Daniel <itzdanielmichael@gmail.com>
2021-02-21 17:37:01 -0500

    lint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
 const App = () => (
   <div className="App">
     <Navbar />
