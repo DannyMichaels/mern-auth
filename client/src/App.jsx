ff0fbf359f48ea7094a8cda6e3f78223df97c35a
Daniel <itzdanielmichael@gmail.com>
2021-02-21 17:41:25 -0500

    update index.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css';
-import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import './App.css';
 
 const App = () => (
   <div className="App">
